a52072808859441fd9cdd00662e6da5265315afe
junior231 <ilocollins231@gmail.com>
2025-08-23 14:12:14 -0400

    Update config for deployment.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // allow unsplash images
    images: {
+    domains: ['images.unsplash.com'],
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
       { protocol: 'https', hostname: 'plus.unsplash.com' },
